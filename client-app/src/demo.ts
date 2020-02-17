d2e85f279af2a8bf2df685c35cbb2e2876f64b4b
Antonio Glešić <aglesic@gmail.com>
2020-02-17 20:58:02 +0100

    typescript demo and Cars list

---
+++ client-app/src/demo.ts
@@ -0,0 +1,27 @@
+﻿let data = 42;
+
+data = 35;
+
+
+export interface ICar {
+	color: string;
+	model: string;
+	topSpeed?: number;
+}
+
+const car1: ICar = {
+	color: 'blue',
+	model: 'BMW'
+};
+
+const car2: ICar = {
+	color: 'red',
+	model: 'Mercedes',
+	topSpeed: 100
+};
+
+export const cars = [car1, car2];
+
+const multiplay = (x: any, y: any) => {
+	(x * y).toString();
+}
